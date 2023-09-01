8fdc648fff54777d09bb199ede463a871b02ce50
Q SYED AJMAL <syedajmalq@gmail.com>
2023-09-01 23:59:20 +0530

    OTP generation module ( COMPLETED )

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Login from './components/login/Login';
+import Login from './components/otp/OTP';
 
 function App() {
   return (
--- src/components/otp/OTP.jsx
+++ src/components/otp/OTP.jsx
@@ -41,7 +41,7 @@ export default function Login
   }
 
   const failure = (error)=>{
-    if(error.request.status==406)
+    if(error.request.status===406)
     {
         setExist(true)
     }
@@ -54,9 +54,9 @@ export default function Login
 
       <div className='max-w-screen-lg  2xl:max-w-screen-xl w-full h-full flex flex-col mx-auto justify-center items-center px-4 md:px-0'>
 
-        <div className='pb-8'>
+        {/* <div className='pb-8'>
                 <p className='text-4xl font-bold  inline-block '>LOGIN</p>
-        </div>
+        </div> */}
 
         <form className=' flex flex-col mx-auto w-full md:w-1/3 border-2 border-gray-600 shadow-lg shadow-gray-400 rounded-3xl p-14 justify-center '
               onSubmit={formik.handleSubmit} method='POST'>
@@ -65,13 +65,13 @@ export default function Login
              <input type="email" name="email" id="email" placeholder='Enter your e-mail' className='p-2 border-2 border-gray-600 rounded-lg mb-2 bg-transparent' onChange={formik.handleChange} value={formik.values.email}/>
 
 
-             <label htmlFor="password" className='text-center pb-4 text-xl'>password</label>
-             <input type="password" name="password" id="password"  placeholder='Enter your password' className='p-2 border-2 border-gray-600 rounded-lg mb-2 bg-transparent' onChange={formik.handleChange} value={formik.values.password}/>
+             {/* <label htmlFor="password" className='text-center pb-4 text-xl'>password</label>
+             <input type="password" name="password" id="password"  placeholder='Enter your password' className='p-2 border-2 border-gray-600 rounded-lg mb-2 bg-transparent' onChange={formik.handleChange} value={formik.values.password}/> */}
              
-             {exist && <p className='font-light  inline-block text-red-500'>User credentials already exists</p>
+             {exist && <p className='font-light  inline-block text-red-500 text-center'>User credentials already exists</p>
 }
 
-             <button type='submit' className='bg-black text-white p-2 mt-5 rounded-lg'>SUBMIT</button>
+             <button type='submit' className='bg-black text-white p-2 mt-5 rounded-lg'>SEND OTP</button>
 
         </form>
 
